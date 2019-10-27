38489421ee3b1c9539ab55cb1fbbb2911d4466f4
Kazushi Kawamura <wakaka2277@gmail.com>
2019-10-27 12:01:21 +0900

    feat: make loader

---
+++ src/index.tsx
@@ -0,0 +1,67 @@
+import React, { useState, useContext } from "react";
+import { View, ActivityIndicator } from "react-native";
+
+export const LoaderContext = React.createContext<any>(null);
+
+export const useLoader = () => {
+  const [loading, setLoading] = useContext(LoaderContext);
+  const showLoader = () => {
+    setLoading(true);
+  };
+  const hideLoader = () => {
+    setLoading(false);
+  };
+
+  return { showLoader, hideLoader };
+};
+
+const Loader = () => {
+  return (
+    <View
+      style={{
+        position: "absolute",
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0,
+        backgroundColor: "#444a",
+        zIndex: 99999,
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
+      <View
+        style={{
+          borderRadius: 4,
+          backgroundColor: "white",
+          width: 100,
+          height: 100,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          boxShadow: "0px 1px 3px rgba(0, 0, 0, 0.1)"
+        }}
+      >
+        <ActivityIndicator size="large" />
+      </View>
+    </View>
+  );
+};
+
+interface Props {
+  children?: React.ReactNode;
+  config?: {};
+}
+
+export const Provider = ({ children }: Props) => {
+  const [loading, setLoading] = useState(true);
+  return (
+    <LoaderContext.Provider value={[loading, setLoading]}>
+      <>
+        {loading && <Loader />}
+        {children}
+      </>
+    </LoaderContext.Provider>
+  );
+};
