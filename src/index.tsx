7de1ca2f4db994fe6824a671dcd639e0dc37b940
Kazushi Kawamura <wakaka2277@gmail.com>
2019-10-27 21:33:05 +0900

    feat: add options

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,21 @@
 import React, { useState, useContext } from "react";
 import { View, ActivityIndicator } from "react-native";
 
-export const LoaderContext = React.createContext<any>(null);
+interface Context {
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const LoaderContext = React.createContext<Context>({
+  setLoading: () => {}
+});
 
 export const useLoader = () => {
-  const [loading, setLoading] = useContext(LoaderContext);
+  const { setLoading } = useContext(LoaderContext);
+
   const showLoader = () => {
     setLoading(true);
   };
+
   const hideLoader = () => {
     setLoading(false);
   };
@@ -15,50 +23,102 @@ export const useLoader = () => {
   return { showLoader, hideLoader };
 };
 
-const Loader = () => {
-  return (
+interface LoaderProps {
+  size: number;
+  backgroundColor: string;
+  foregroundColor: string;
+  spinnerColor: string;
+  cornerRadius: number;
+}
+
+const Loader = ({
+  size,
+  backgroundColor,
+  foregroundColor,
+  spinnerColor,
+  cornerRadius
+}: LoaderProps) => (
+  <View
+    style={{
+      position: "absolute",
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0,
+      backgroundColor: backgroundColor,
+      zIndex: 99999,
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center"
+    }}
+  >
     <View
       style={{
-        position: "absolute",
-        top: 0,
-        right: 0,
-        bottom: 0,
-        left: 0,
-        backgroundColor: "#444a",
-        zIndex: 99999,
+        borderRadius: cornerRadius,
+        backgroundColor: foregroundColor,
+        width: size,
+        height: size,
         display: "flex",
         justifyContent: "center",
         alignItems: "center"
       }}
     >
-      <View
-        style={{
-          borderRadius: 4,
-          backgroundColor: "white",
-          width: 100,
-          height: 100,
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center"
-        }}
-      >
-        <ActivityIndicator size="large" />
-      </View>
+      <ActivityIndicator color={spinnerColor} size="large" />
     </View>
-  );
-};
+  </View>
+);
 
-interface Props {
-  children?: React.ReactNode;
-  config?: {};
+export interface OptionalConfig {
+  size?: number;
+  backgroundColor?: string;
+  foregroundColor?: string;
+  spinnerColor?: string;
+  cornerRadius?: number;
 }
 
-export const Provider = ({ children }: Props) => {
+interface Config {
+  size: number;
+  backgroundColor: string;
+  foregroundColor: string;
+  spinnerColor: string;
+  cornerRadius: number;
+}
+
+export interface ProviderProps {
+  children?: React.ReactNode;
+  config?: OptionalConfig;
+}
+
+const defaultConfig: Config = {
+  size: 100,
+  backgroundColor: "#444a",
+  foregroundColor: "#fff",
+  spinnerColor: "gray",
+  cornerRadius: 4
+};
+
+export const Provider = ({ children, config }: ProviderProps) => {
   const [loading, setLoading] = useState(false);
+  const {
+    size,
+    backgroundColor,
+    foregroundColor,
+    spinnerColor,
+    cornerRadius
+  } = { ...defaultConfig, ...config };
+
   return (
-    <LoaderContext.Provider value={[loading, setLoading]}>
+    <LoaderContext.Provider value={{ setLoading }}>
       <>
-        {loading && <Loader />}
+        {loading && (
+          <Loader
+            size={size}
+            backgroundColor={backgroundColor}
+            foregroundColor={foregroundColor}
+            spinnerColor={spinnerColor}
+            cornerRadius={cornerRadius}
+          />
+        )}
         {children}
       </>
     </LoaderContext.Provider>
